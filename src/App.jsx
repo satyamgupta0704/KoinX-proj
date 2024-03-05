db5b05539ad614a605a4646361d5605188e15060
satyamgupta0704 <103809254+satyamgupta0704@users.noreply.github.com>
2024-03-05 23:38:49 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import NotFound from "pages/NotFound";
+import Bitcoin from "pages/Bitcoin";
+
+function App() {
+  return (
+    <Bitcoin />
+  );
+}
+
+export default App;
